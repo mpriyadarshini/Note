1c5a60300b205b9e93d1be5a222a04293b59fb47
Megha Priyadarshini <meghapriyadarshini68@gmail.com>
2019-12-04 18:28:40 +0530

    Create sidebar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import Note from './Note';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sidebar">
+        <Note title="Jenkins" date="21/09/1997" />
+        <Note title="Mengha" date="1/09/1997" />
+        <Note title="Chenga" date="2/09/1997" />
+      </div>
+      <div className="content">
+
+      </div>
     </div>
+
   );
 }
 
---
+++ src/Note.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Note.css';
+
+function Note({ title, date }) {
+    return (
+        <div className="note">
+            <div className="heading">
+                {title}
+            </div>
+            <p className="time">{date}</p>
+        </div>
+    )
+}
+export default Note;
